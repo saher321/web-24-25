1cdd04354711fa8090cbc8c9173ea3e4bc5a2595
pny <pnymeet@gmail.com>
2025-10-19 17:09:53 +0500

    Backend notes db connected

---
+++ web/mern/mernapp1/server/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    const db = await mongoose.connect(process.env.MONGO_URI);
+    if (db) {
+      console.log(`Database is connected: ${db.connection.host}`)
+    } else {
+      console.log(`Database isn't connected`)
+    }
+  } catch (error) {
+    console.log(`Something went wrong: ${error}`)
+  }
+}
--- web/mern/mernapp1/server/server.js
+++ web/mern/mernapp1/server/server.js
@@ -1,21 +1,28 @@
 // express, dotenv, cors, nodemon, jsonwebtoken
 import express from 'express';
 import dotenv from 'dotenv';
-
+import { connectDB } from './config/db.js'
+import Notes from './models/notesModels.js';
 dotenv.config();
 const app = express();
 
 // 3000, 5000, 7000, 8000, 8080
-const PORT = process.env.PORT || 5000
+const PORT =  5000
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is running http://localhost:${PORT}/`);
 });
 
 app.get('/', (req, res) => {
   res.send({ name: 'Murtaza', email: 'mu87683@gmail.com' });
 });
-// assignment:: Products: id, title, shortDescription, price, rating(4,5,2), productCategory
+
+app.get('/notes', async (req, res) => {
+  const notes = await Notes.find({});
+  return res.send({status: true, notes})
+})
+// assignment:: 10 Products: id, title, shortDescription, price, rating(4,5,2), productCategory
 const roles = [
   { id: 101, rolename: 'Super Admin' },
   { id: 102, rolename: 'Admin' },
